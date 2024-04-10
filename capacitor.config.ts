2d9c462f9a50aa5e7e030b3ce5c2594180a5188a
hewham <hamiltonhew@gmail.com>
2024-04-09 22:04:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'anonacy-vue',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
